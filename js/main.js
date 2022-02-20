c940c3c1c940745464b05d716db253a02cc06406
ubkbek <ulugbekjovliyev01001@gmail.com>
2022-02-21 02:16:16 +0500

    add js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+alert("So'rovnomani toldiring!!!");
+
+var ism = prompt("Ismingizni kiriting");
+var surname = prompt("Familiyangizni kiriting");
+var phoneNumber = prompt("Raqamingizni kiriting");
+var email = prompt("Elektron pochtangzini kiriting");
+var age = prompt("Yoshingizni kiriting", "20");
+var hobby = prompt("Nimalarga qiziqasiz");
+var study = prompt("O'qiysizmi");
+var work = prompt("Ishlaysizmi?");
+var ogirlik = prompt("Vazningiz qancha");
+var boy = prompt("Bo'yingiz qancha?");
+var address = prompt("Manzilingiz");
+
+
+alert("ism : " + ism + "\n" + "familiya : " + surname + "\n" + "telefon : " + phoneNumber + "\n" + "email : " + email + "\n" + "age : " + age + "\n" + "hobby : " + hobby + "\n" + "o'qish : " + study + "\n" + "ishlaysizmi : " + work + "\n" + "ogirlik :" + ogirlik + "\n" + "boy : " + boy + "\n" +"manzil : " + address);
+
+
+console.log("ism : " + ism + "\n" + "familiya : " + surname + "\n" + "telefon : " + phoneNumber + "\n" + "email : " + email + "\n" + "age : " + age + "\n" + "hobby : " + hobby + "\n" + "o'qish : " + study + "\n" + "ishlaysizmi : " + work + "\n" + "ogirlik :" + ogirlik + "\n" + "boy : " + boy + "\n" +"manzil : " + address);
